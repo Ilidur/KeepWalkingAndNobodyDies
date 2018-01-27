df7aa69aa64691c3bf9bba0568c8c257a6da5965
Japke <japke.karreman@hotmail.com>
2018-01-27 21:23:33 +0000

    Detection of objects
    Added scripts that detect when the objects go into a certain area and then changes the colour of the objects and count how many are in the area

---
+++ KeepWalkingAndNobodyDies/Assets/DetectingOtherObject.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectingOtherObject : MonoBehaviour {
+
+    public Color color;
+    public Type objectType;
+    public List<PuzzleProperties> list;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PuzzleProperties properties = other.gameObject.GetComponent<PuzzleProperties>();
+        if (properties != null)
+        {
+            Debug.Log("Entered the trigger field");
+            list.Add(properties);
+            if (objectType == properties.objectType)
+            {
+                if (properties.color == Color.blue)
+                {
+                    properties.color = Color.red;
+                    other.gameObject.GetComponent<Renderer>().material.color = Color.red;
+                }
+                else
+                {
+
+                    properties.color = Color.blue;
+                    other.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+                }
+            }
+
+            if (list.Count > 1)
+            {
+                Debug.Log("More than one!");
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        PuzzleProperties properties = other.gameObject.GetComponent<PuzzleProperties>();
+        if (properties != null)
+        {
+            Debug.Log("Left the trigger field");
+            list.Remove(properties);
+            if (objectType == properties.objectType)
+            {
+                if (properties.color == Color.blue)
+                {
+                    properties.color = Color.green;
+                    other.gameObject.GetComponent<Renderer>().material.color = Color.green;
+                }
+                else
+                {
+
+                    properties.color = Color.blue;
+                    other.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+                }
+            }
+            if (list.Count < 2)
+            {
+                Debug.Log("One or less!");
+            }
+        }
+    }
+}
---
+++ KeepWalkingAndNobodyDies/Assets/PuzzleProperties.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Type
+{
+    Cube,Sphere
+}
+
+public class PuzzleProperties : MonoBehaviour {
+
+    public Color color;
+    public Type objectType;
+
+    // Use this for initialization
+    void Start () {
+        color = gameObject.GetComponent<Renderer>().material.color;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
